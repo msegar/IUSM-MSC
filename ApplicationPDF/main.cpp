4f6938afbd6f4b44d7b5d441b5649ed1dfa6c5e8
Matt Segar <Matt@Matts-rMBP.local>
2016-01-27 19:16:59 -0500

    Modified parser for Jotform application instead of Google Form.

--- ApplicationPDF/main.cpp
+++ ApplicationPDF/main.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <fstream>
+#include <iostream>     // cout, endl
+#include <fstream>      // fstream
 #include <vector>
+#include <string>
+#include <algorithm>    // copy
+#include <iterator>     // ostream_operator
 #include <stdlib.h>
-#include <sstream>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+
+#include <boost/tokenizer.hpp>
 
 using namespace std;
+using namespace boost;
 
 string getCommittee(string com){
 	string committee = "";
@@ -19,85 +21,42 @@ string getCommittee(string com){
 	return committee;
 }
 
-int main(int argc, const char* argv[])
-{
-	if (argc < 2){
+int main(int argc, const char* argv[]){   
+    if (argc < 2){
 		cout << "usage: [exe] [applicant txt]" << endl;
 		exit(1);
 	}
-	
-	ifstream input;
-	input.open(argv[1]);
-	
-	vector< vector<string> > db;
-	vector<string> curr;
-	bool first = true;
-	string longWord = "";
-	bool lw = false;
-	
-	for (string row; getline(input, row);){
-		if (first){
-			first = false;
-			continue;
-		}
-		istringstream ss(row);
-		if (lw == false)
-			curr.clear();
-		for (string word; getline(ss, word, '\t');){
-			cout << "word: " << word << endl;
-			if (count(word.begin(), word.end(), '"') == 2)
-				curr.push_back(word);
-			else if (word[0] == '"'){
-				cout << "case1" << endl;
-				longWord = word;
-				longWord += "\n";
-				lw = true;
-			} else if ((lw == true) && (word.find("\"") == string::npos)){
-				cout << "case2" << endl;
-				longWord += word;
-				longWord += "\n";
-			} else if ((lw == true) && (word.find("\"") != string::npos)){
-				cout << "case3" << endl;
-				longWord += word;
-				curr.push_back(longWord);
-				cout << "size: " << curr.size() << endl;
-				longWord = "";
-				lw = false;
-			} else if (lw == false){
-				cout << "case4" << endl;
-				curr.push_back(word);
-				cout << "size: " << curr.size() << endl;
-			}
-		}
-		if (lw == false){
-			//if ((curr.size() < 40) && (curr.size() < 2)){
-			if (curr.size() != 42){
-				cout << "**ERROR: less than 42" << endl;
-				cout << "size: " << curr.size() << endl;
-				cout << "Line/applicant " << db.size()+1 << endl;
-				cout << curr[0] << endl;
-				exit(1);
-			}
-			db.push_back(curr);
-		}
 
-	}
+    ifstream in;
+	in.open(argv[1]);
 	
-	cout << "# of applicants: " << db.size() << endl;
+    if (!in.is_open()) return 1;
+
+    typedef tokenizer< escaped_list_separator<char> > Tokenizer;
+
+	bool first = true;
+    vector< string > vec;
+    string line;
+    vector< vector<string> > db;
+    vector<string> curr;
+    int t;
+
+    while (getline(in,line))
+    {
+        Tokenizer tok(line);
+        vec.assign(tok.begin(),tok.end());
+
+        if (vec.size() < 48) continue;
+        if (first){
+        	first = false;
+        	continue;
+        }
+		db.push_back(vec);
+		vec.clear();
+    }
+    cout << "# of applicants: " << db.size() << endl;
 	
-	// loop through database removing quotation marks
-	for (int i = 0; i < db.size(); ++i){
-		for (int j = 0; j < db[i].size(); ++j){
-			string q = db[i].at(j);
-			//cout << q << endl;
-			q.erase(remove(q.begin(), q.end(), '"'), q.end());
-			db[i].at(j) = q;
-			//cout << q << endl;
-			//cout << endl;
-		}
-	}
-	
-	cout << "cleaned quotations" << endl;
+	//cin >> t;
 	
 	bool repeat = true;
 	
@@ -107,16 +66,17 @@ int main(int argc, const char* argv[])
 		while (repeat){
 			curr.clear();
 			cout << "copy size: " << db[i].size() << endl;
-			for (int j = 0; j < 20; ++j){
+			for (int j = 1; j < 26; ++j){
 				cout << j << ": " << db[i].at(j) << endl;
 				curr.push_back(db[i].at(j));
 			}
 		
+			// manually add these 2 columns to the end of the spreadsheet
 			// add last 2 elements to vector
 			curr.push_back(db[i].at(db[i].size()-2)); // phone?
 			curr.push_back(db[i].at(db[i].size()-1)); // skype username
 
-			string committee = getCommittee(curr[15]);
+			string committee = getCommittee(curr[20]);
 			cout << committee << endl;
 	
 			string name = curr[0];
@@ -133,22 +93,23 @@ int main(int argc, const char* argv[])
 			command += "\"";
 			
 			// loop through adding cells to command
-			for (int j = 2; j < 20; ++j){
+			for (int j = 2; j < 25; ++j){
 				command += " ";
 				command += "\"";
 				command += curr[j];
 				command += "\"";
 			}
 	
-			//cout << command << endl;
+			cout << command << endl;
+			//cin >> t;
 		
-			cout << "20: " << curr[curr.size()-2] << endl;
+			cout << "26: " << curr[curr.size()-2] << endl;
 			command += " ";
 			command += "\"";
 			command += curr[curr.size()-2]; // phone interview?
 			command += "\"";
 
-			cout << "21: " << curr[curr.size()-1] << endl;			
+			cout << "27: " << curr[curr.size()-1] << endl;			
 			command += " ";
 			command += "\"";
 			command += curr[curr.size()-1]; // skype username
@@ -170,17 +131,14 @@ int main(int argc, const char* argv[])
 		
 			cout << "command: " << command << endl;
 			system(command.c_str());
-			
-			int t;
-			//cin >> t;
-			
+			//cin>>t;
 			// apply to another?
 			if (curr[curr.size()-3].compare("Yes") == 0){
-				db[i].erase(db[i].begin()+15);
-				db[i].erase(db[i].begin()+15);
-				db[i].erase(db[i].begin()+15);
-				db[i].erase(db[i].begin()+15);
-				db[i].erase(db[i].begin()+15);
+				db[i].erase(db[i].begin()+21);
+				db[i].erase(db[i].begin()+21);
+				db[i].erase(db[i].begin()+21);
+				db[i].erase(db[i].begin()+21);
+				db[i].erase(db[i].begin()+21);
 				repeat = true;
 			} else
 				repeat = false;
